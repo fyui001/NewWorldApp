82d28188076210ec62cf0f7857fc57684c980c9a
fyui001 <yun.cod@gmail.com>
2020-11-03 16:47:43 +0900

    ロゴを追加

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('top_page') }}" class="brand-link">
-        <img src="/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="/img/new_world_logo.png" alt="NewWorld Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">管理画面</span>
     </a>
     <div class="sidebar">
