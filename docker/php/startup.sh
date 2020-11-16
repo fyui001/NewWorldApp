0d993ae82a481f17a8306adac1b300ca4b8bd287
fyui001 <yun.cod@gmail.com>
2020-11-17 00:13:35 +0900

    db操作をスクリプトから消した

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -14,10 +14,6 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-# database mingrate
-php artisan migrate
-php artisan db:seed
-
 chmod -R 777 /code/storage bootstrap/cache
 
 echo "starting php-fpm"
