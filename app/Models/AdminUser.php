0f50ced349d1e81e2cb93e86cc0f8bfe4b6f905d
fyui001 <yun.cod@gmail.com>
2020-12-13 23:38:36 +0900

    管理画面の認証がぶっ壊れてたので直した

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Models\Model as AppModel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
