0357c2256989d28161b8e92bc19f0373b5449955
fyui001 <yun.cod@gmail.com>
2020-12-14 23:26:56 +0900

    ユーザーモデルを修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,24 +4,51 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Models\Model as AppModel;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends AppModel
+class User extends Authenticatable implements JWTSubject
 {
 
     protected $table = 'users';
 
     protected $fillable = [
+        'user_id',
         'name',
         'password',
         'is_registered',
         'del_flg',
     ];
 
-    public function MedicationHistory() {
+    protected $hidden = [
+        'password',
+    ];
 
-        return $this->hasMany('App\Model\MedicationHistory', 'drug_id');
+    public function medicationHistories() {
+
+        return $this->hasMany('App\Models\MedicationHistory', 'user_id');
 
     }
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+
+        return $this->getKey();
+
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+
+        return [];
+
+    }
 }
