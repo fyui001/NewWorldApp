14cdd11348868557e4c687381ed0e3a56f5ee260
fyui001 <yun.cod@gmail.com>
2022-06-16 01:06:48 +0900

    fix typo

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -10,7 +10,7 @@ class RepositoryServiceProvider extends ServiceProvider
 {
     public $bindings = [
         Domain\AdminUser\AdminUserRepository::class => EloquentRepository\AdminUserRepository::class,
-        Domain\Drugs\DrugRepository::class => EloquentRepository\DrugRepository::class,
+        Domain\Drug\DrugRepository::class => EloquentRepository\DrugRepository::class,
         Domain\MedicationHistory\MedicationHistoryRepository::class => EloquentRepository\MedicationHistoryRepository::class,
         Domain\User\UserRepository::class => EloquentRepository\UserRepository::class,
     ];
