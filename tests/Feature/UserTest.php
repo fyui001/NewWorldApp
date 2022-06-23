958a0fde334e10f50f248a6439b31c7dfae67527
fyui001 <yun.cod@gmail.com>
2022-06-23 21:36:31 +0900

    テストを微調整

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,12 +1,10 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -14,26 +12,20 @@ class UserTest extends TestCase
 
     use RefreshDatabase;
 
-    public function testLoginSuccess() {
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->createUser();
+    }
 
-        DB::table('users')->insert([
-            'user_id' => 19890308,
-            'name' => '松井恵理子',
-            'icon_url' => '',
-            'password' => Hash::make('hogehoge'),
-            'access_token' => '',
-            'is_registered' => 1,
-            'del_flg' => 0,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
+    public function testLoginSuccess()
+    {
         $params = [
             'user_id' => 19890308,
             'password' => 'hogehoge',
         ];
 
-        $response = $this->json('POST', route('admin.users.login'), $params);
+        $response = $this->json('POST', route('api.users.login'), $params);
         $response->assertStatus(200)->assertJson([
             'status' => true,
             'data' => $response['data'],
@@ -42,29 +34,19 @@ class UserTest extends TestCase
     }
 
     public function testLoginFailure() {
-        DB::table('users')->insert([
-            'user_id' => 19890308,
-            'name' => '松井恵理子',
-            'icon_url' => '',
-            'password' => Hash::make('hogehoge'),
-            'access_token' => '',
-            'is_registered' => 1,
-            'del_flg' => 0,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
         $params = [
             'user_id' => 19890308,
             'password' => 'testtest',
         ];
 
-        $response = $this->json('POST', route('admin.users.login'), $params);
+        $response = $this->json('POST', route('api.users.login'), $params);
         $response->assertStatus(400)->assertJson([
             'status' => false,
-            'msg' => 'unauthorized',
+            'message' => 'Login Failure',
+            'errors' => [
+                'type' => 'login_failure',
+            ],
+            'data' => null,
         ]);
-
     }
-
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,58 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function createAdminUser(): void
+    {
+        DB::table('admin_users')->insert([
+            'user_id' => 'takada_yuki',
+            'password' => Hash::make('hogehoge'),
+            'name' => '高田憂希',
+            'role' => 1,
+            'status' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    public function createDrug(): void
+    {
+        DB::table('drugs')->insert([
+            'drug_name' => 'フルニトラゼパム',
+            'url' => 'https://ja.wikipedia.org/wiki/%E3%83%95%E3%83%AB%E3%83%8B%E3%83%88%E3%83%A9%E3%82%BC%E3%83%91%E3%83%A0',
+        ]);
+    }
+
+    public function createMedicationHistory(): void
+    {
+        DB::table('medication_histories')->insert([
+            'user_id' => 1,
+            'drug_id' => 1,
+            'amount' => 2,
+        ]);
+    }
+
+    public function createUser(): void
+    {
+        DB::table('users')->insert([
+            'user_id' => '19890308',
+            'name' => '松井恵理子',
+            'icon_url' => '',
+            'password' => Hash::make('hogehoge'),
+            'access_token' => '',
+            'status' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
 }
