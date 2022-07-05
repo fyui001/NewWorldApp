28a1ff0144967317e5df76155fad791992d21633
fyui001 <yun.cod@gmail.com>
2022-07-06 08:48:08 +0900

    WIP: fix bug

--- tests/Feature/Api/UserUserControllerTest.php
+++ tests/Feature/Api/UserUserControllerTest.php
@@ -14,7 +14,6 @@ class UserUserControllerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->createUser();
     }
 
     public function testLoginSuccess()
