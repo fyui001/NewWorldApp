a369b26711b4b34ea7baa58b556f06d4e83e0b5c
fyui001 <yun.cod@gmail.com>
2022-07-07 21:27:17 +0900

    fix Drug controller test

--- tests/Feature/Api/DrugControllerTest.php
+++ tests/Feature/Api/DrugControllerTest.php
@@ -32,4 +32,45 @@ class DrugControllerTest extends TestCase
 
         $response->assertStatus(200)->assertJson($except);
     }
+
+    public function testCreate()
+    {
+        $this->userLogin();
+
+        $params = [
+            'drug_name' => 'ゾルピデム',
+            'url' => 'https://ja.wikipedia.org/wiki/%E3%82%BE%E3%83%AB%E3%83%94%E3%83%87%E3%83%A0',
+        ];
+
+        $response = $this->json('POST', route('api.drugs.create'), $params);
+
+        $except = [
+            'status' => true,
+            'message' => '',
+            'data' => null,
+        ];
+
+        $response->assertStatus(200)
+            ->assertJson($except);
+    }
+
+    public function testShow()
+    {
+        $this->userLogin();
+
+        $params = [
+            'drug_name' => 'フルニトラゼパム',
+        ];
+
+        $response = $this->json('GET', route('api.drugs.show'), $params);
+
+        $except = [
+            'status' => true,
+            'message' => '',
+            'data' => $this->drug->toArray(),
+        ];
+
+        $response->assertStatus(200)
+            ->assertJson($except);
+    }
 }
