a458eab1567eed22795b2192d02195fa27e53ce3
fyui001 <yun.cod@gmail.com>
2022-07-10 01:04:55 +0900

    refactor: Api UserControllerTest

--- tests/Feature/Api/UserControllerTest.php
+++ tests/Feature/Api/UserControllerTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api;
 
+use Domain\User\UserStatus;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Infra\EloquentModels\User;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
@@ -46,5 +48,73 @@ class UserControllerTest extends TestCase
             ],
             'data' => null,
         ]);
+    }
+
+    public function testShow()
+    {
+        $this->userLogin();
+
+        $response = $this->json('GET', route('api.users.show'));
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+            ]);
+    }
+
+    public function testRegister()
+    {
+        $params = [
+            'user_id' => 19890308,
+            'password' => 'matsui_erico',
+            'password_confirm' => 'matsui_erico'
+        ];
+
+        $response = $this->json('POST', route('api.users.register'), $params);
+        $response->assertStatus(409)
+            ->assertJson([
+                'status' => false,
+                'message' => 'Duplicate entry',
+                'errors' => [
+                    'type' => 'duplicate_entry'
+                ],
+                'data' => null,
+            ]);
+
+        $params = [
+            'user_id' => 930316,
+            'password' => 'takada_yuki',
+            'password_confirm' => 'takada_yuki',
+        ];
+
+        $response = $this->json('POST', route('api.users.register'), $params);
+        $response->assertStatus(404)
+            ->assertJson([
+                'status' => false,
+                'message' => '404 Notfound.',
+                'errors' => [
+                    'type' => 'not_found',
+                ],
+                'data' => null,
+            ]);
+
+        $model = new User();
+
+        $model->user_id = 930316;
+        $model->name = '高田憂希';
+        $model->icon_url = '';
+        $model->password = '';
+        $model->status = UserStatus::STATUS_UNREGISTERED->getValue()->getRawValue();
+
+        $model->save();
+
+        $response = $this->json('POST', route('api.users.register'), $params);
+        $response->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+                'message' => 'registered',
+                'errors' => null,
+                'data' => null,
+            ]);
     }
 }
